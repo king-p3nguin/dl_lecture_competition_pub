b54a7c93e140e2b324626442cf4b757e15d16403
king-p3nguin <kazukitsuoka@g.ecc.u-tokyo.ac.jp>
2024-07-16 07:53:04 +0900

    change: update models

--- src/datasets.py
+++ src/datasets.py
@@ -3,6 +3,7 @@ from typing import Tuple
 
 import numpy as np
 import torch
+import torchvision
 from termcolor import cprint
 
 
--- src/models.py
+++ src/models.py
@@ -5,10 +5,8 @@ import sys
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torchaudio.transforms as T
+import torchvision.transforms as T
 from einops.layers.torch import Rearrange
-
-from transform import CWT
 
 logger = logging.getLogger(__name__)
 
@@ -20,15 +18,14 @@ class BasicConvClassifier(nn.Module):
         super().__init__()
 
         self.blocks = nn.Sequential(
-            ConvBlock(in_channels, hid_dim, p_drop=0.5),
-            ConvBlock(hid_dim, hid_dim * 2, p_drop=0.5),
-            ConvBlock(hid_dim * 2, hid_dim * 3, p_drop=0.5),
+            ConvBlock(in_channels, hid_dim),
+            ConvBlock(hid_dim, hid_dim),
         )
 
         self.head = nn.Sequential(
             nn.AdaptiveAvgPool1d(1),
             Rearrange("b d 1 -> b d"),
-            nn.Linear(hid_dim * 3, num_classes),
+            nn.Linear(hid_dim, num_classes),
         )
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
@@ -87,47 +84,38 @@ class NewConvBlock(nn.Module):
         self,
         in_dim,
         out_dim,
-        kernel_size: int = 5,
+        kernel_size: int = 3,
         p_drop: float = 0.1,
     ) -> None:
-        logger.debug(f"NewConvBlock: in_dim={in_dim}, out_dim={out_dim}")
         super().__init__()
 
         self.in_dim = in_dim
         self.out_dim = out_dim
 
-        self.conv0 = nn.Conv2d(in_dim, out_dim, kernel_size)
-        self.conv1 = nn.Conv2d(out_dim, out_dim, kernel_size)
-        self.conv2 = nn.Conv2d(out_dim, out_dim, kernel_size)
+        self.conv0 = nn.Conv1d(in_dim, out_dim, kernel_size, padding="same")
+        self.conv1 = nn.Conv1d(out_dim, out_dim, kernel_size, padding="same")
+        # self.conv2 = nn.Conv1d(out_dim, out_dim, kernel_size) # , padding="same")
 
-        self.batchnorm0 = nn.BatchNorm2d(num_features=out_dim)
-        self.batchnorm1 = nn.BatchNorm2d(num_features=out_dim)
-        self.batchnorm2 = nn.BatchNorm2d(num_features=out_dim)
-
-        self.avgpool = nn.AvgPool2d(2)
+        self.batchnorm0 = nn.BatchNorm1d(num_features=out_dim)
+        self.batchnorm1 = nn.BatchNorm1d(num_features=out_dim)
 
         self.dropout = nn.Dropout(p_drop)
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
-        X = self.conv0(X)
+        if self.in_dim == self.out_dim:
+            X = self.conv0(X) + X  # skip connection
+        else:
+            X = self.conv0(X)
+
         X = F.gelu(self.batchnorm0(X))
-        X = self.avgpool(X)
-        X = self.dropout(X)
-        logger.debug(f"NewConvBlock layer1: {X.shape}")
 
-        X = self.conv1(X)
+        X = self.conv1(X) + X  # skip connection
         X = F.gelu(self.batchnorm1(X))
-        X = self.avgpool(X)
-        X = self.dropout(X)
-        logger.debug(f"NewConvBlock layer2: {X.shape}")
 
-        X = self.conv2(X)
-        X = F.gelu(self.batchnorm2(X))
-        X = self.avgpool(X)
-        X = self.dropout(X)
-        logger.debug(f"NewConvBlock layer3: {X.shape}")
+        # X = self.conv2(X)
+        # X = F.glu(X, dim=-2)
 
-        return X
+        return self.dropout(X)
 
 
 class NewConvClassifier(nn.Module):
@@ -135,31 +123,20 @@ class NewConvClassifier(nn.Module):
         self, num_classes: int, seq_len: int, in_channels: int, hid_dim: int = 64
     ) -> None:
         super().__init__()
-        self.seq_len = seq_len
-        self.in_channels = in_channels
 
-        # dim = [channel, 94, 94]
-        self.spec = nn.Sequential(
-            # T.Spectrogram(n_fft=186, win_length=32, hop_length=3, normalized=True),
-            # T.AmplitudeToDB(),
-            CWT(hop_length=2),
+        self.blocks = nn.Sequential(
+            T.Normalize(mean=0.5, std=0.5),  # to avoid nan during training
+            ConvBlock(in_channels, hid_dim, p_drop=0.5),
+            ConvBlock(hid_dim, hid_dim * 2, p_drop=0.5),
+            ConvBlock(hid_dim * 2, hid_dim * 3, p_drop=0.5),
+            ConvBlock(hid_dim * 3, hid_dim * 4, p_drop=0.5),
         )
-
-        self.blocks = NewConvBlock(in_channels, hid_dim)
 
         self.head = nn.Sequential(
-            nn.Flatten(start_dim=1),
-            nn.Linear(self.feature_dim(), num_classes, bias=False),
+            nn.AdaptiveMaxPool1d(1),
+            Rearrange("b d 1 -> b d"),
+            nn.Linear(hid_dim * 4, num_classes),
         )
-
-    def feature_dim(self):
-        with torch.no_grad():
-            mock_tensor = torch.zeros(1, self.in_channels, self.seq_len)
-
-            mock_tensor = self.spec(mock_tensor)
-            mock_tensor = self.blocks(mock_tensor)
-
-        return mock_tensor.shape[1] * mock_tensor.shape[2] * mock_tensor.shape[3]
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
         """_summary_
@@ -168,15 +145,9 @@ class NewConvClassifier(nn.Module):
         Returns:
             X ( b, num_classes ): _description_
         """
-        X = self.spec(X)  # (b, c, win_length+1, t/hop_length)
-        logger.debug("spec: %s", X.shape)
-
         X = self.blocks(X)
-        logger.debug("blocks: %s", X.shape)
 
-        X = self.head(X)
-        logger.debug("head: %s", X.shape)
-        return X
+        return self.head(X)
 
 
 class Conv2dWithConstraint(nn.Conv2d):
